f11fea4f966da945850f8e8aca19cb1fc5bcef57
Amanda Amorim <amandagadelhaamorim@gmail.com>
2022-06-01 11:01:32 -0300

    refatoramento seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Coin.create!(
-  description: "Bitcoin",
-  acronym: "BTN",
-  url_image: "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png"
-)
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas...")
+    spinner.auto_spin
 
-Coin.create!(
-  description: "Ethereum",
-  acronym: "ETH",
-  url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/628px-Ethereum_logo_2014.svg.png"
-)
+coins = [
+      { description: "Bitcoin",
+        acronym: "BTN",
+        url_image: "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png"
+      },
 
-Coin.create!(
-  description: "Dash",
-  acronym: "DASH",
-  url_image: "https://www.pngkit.com/png/detail/135-1353048_dash-icon-dash-coin-logo-png.png"
-)
+      { description: "Ethereum",
+        acronym: "ETH",
+        url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/628px-Ethereum_logo_2014.svg.png"
+      },
+
+      { description: "Dash",
+        acronym: "DASH",
+        url_image: "https://www.pngkit.com/png/detail/135-1353048_dash-icon-dash-coin-logo-png.png"
+      },
+    ]
+
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Concluído)")
