7877ece5f4c781246a1cb094936514e0a457214b
Amanda Amorim <amandagadelhaamorim@gmail.com>
2022-06-01 11:49:38 -0300

    corrigindo o MinigType para description: no lugar de name:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_141519) do
+ActiveRecord::Schema.define(version: 2022_06_01_144554) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_06_01_141519) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
